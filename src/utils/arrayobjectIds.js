6f5fe8a99c08892b646ca263c415e242eef19a01
Elsamanoudy74 <161150074+Elsamanoudy74@users.noreply.github.com>
2024-03-06 03:40:58 +0200

    Delete src directory

--- src/utils/arrayobjectIds.js
+++
@@ -1,6 +0,0 @@
-import { Types } from "mongoose";
-
-export const arrayofIds = (arrayIds) => {
-  const resultArrayIds = arrayIds.map((key) => new Types.ObjectId(key));
-  return resultArrayIds;
-};
